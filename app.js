724bdc812fc8f64acbc2833068df5b53a6e78844
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2023-11-15 14:37:54 -0600

    Set index route

--- app.js
+++ app.js
@@ -6,4 +6,9 @@ const PORT = process.env.PORT || 3500;
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
