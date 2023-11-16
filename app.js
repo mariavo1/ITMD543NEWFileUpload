f64ffcfa2a1ad119fd9613c5a2edaa6230a5c179
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2023-11-15 18:49:24 -0600

    remove size limit

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const path = require("path");
 
 const filesPayloadExists = require('./middleware/filesPayloadExists');
 const fileExtLimiter = require('./middleware/fileExtLimiter');
-const fileSizeLimiter = require('./middleware/fileSizeLimiter');
 
 const PORT = process.env.PORT || 3500;
 
@@ -18,7 +17,6 @@ app.post('/upload',
     fileUpload({ createParentPath: true }),
     filesPayloadExists,
     fileExtLimiter(['.png', '.jpg', '.jpeg']),
-    fileSizeLimiter,
     (req, res) => {
         const files = req.files
 
